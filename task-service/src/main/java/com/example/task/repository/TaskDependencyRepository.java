10a6b4a477f2cb9a24d16a450bc72c3f71c578d7
phyc11 <xuanphuc111104@gmail.com>
2026-09-16 16:44:02 +0700

    Update task

--- task-service/src/main/java/com/example/task/repository/TaskDependencyRepository.java
+++ task-service/src/main/java/com/example/task/repository/TaskDependencyRepository.java
@@ -3,4 +3,5 @@ import com.example.task.model.TaskDependency; import org.springframework.data.jp
 public interface TaskDependencyRepository extends JpaRepository<TaskDependency,Long>{
  List<TaskDependency> findByTaskId(Long taskId); boolean existsByTaskIdAndDependsOnTaskId(Long taskId,Long dependsOnTaskId);
  void deleteByTaskIdAndDependsOnTaskId(Long taskId,Long dependsOnTaskId);
+ void deleteByTaskId(Long taskId);
 }
