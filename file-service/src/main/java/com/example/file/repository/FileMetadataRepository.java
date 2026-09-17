0fb9a7d70b1b7a123e19f1f300d7b941ae1cf91d
phyc11 <xuanphuc111104@gmail.com>
2026-09-17 09:30:40 +0700

    Update file service

--- file-service/src/main/java/com/example/file/repository/FileMetadataRepository.java
+++ file-service/src/main/java/com/example/file/repository/FileMetadataRepository.java
@@ -6,9 +6,12 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.time.LocalDateTime;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 @Repository
 public interface FileMetadataRepository extends JpaRepository<FileMetadata, Long> {
     List<FileMetadata> findByEntityTypeAndEntityId(String entityType, Long entityId);
+    Page<FileMetadata> findByEntityTypeAndEntityIdAndWorkspaceId(String entityType,Long entityId,Long workspaceId,Pageable pageable);
     List<FileMetadata> findByUploadedAtBefore(LocalDateTime cutoff);
 }
